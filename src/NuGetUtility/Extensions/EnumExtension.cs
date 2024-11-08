2646761d4cbfcfb91b52fb3649d65024a1a212da
Pierre-Yves Menguy <pymenguy@quantelmedical.fr>
2024-11-08 16:20:46 +0100

    Add OutputColumnType,
    an extension method to get the Description,
    and associated tests

---
+++ src/NuGetUtility/Extensions/EnumExtension.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the projects contributors.
+// The license conditions are provided in the LICENSE file located in the project root
+
+using System.ComponentModel;
+using System.Reflection;
+
+namespace NuGetUtility.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string? GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            if (Enum.GetName(type, value) is not string name)
+            {
+                return null;
+            }
+
+            if (type.GetField(name) is not FieldInfo field)
+            {
+                return null;
+            }
+
+            if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+            {
+                return attr.Description;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ src/NuGetUtility/OutputColumnType.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the projects contributors.
+// The license conditions are provided in the LICENSE file located in the project root
+
+using System.ComponentModel;
+
+namespace NuGetUtility;
+
+public enum OutputColumnType
+{
+    [Description("Package")]
+    Package,
+    PackageId = Package, // Json name
+    [Description("Version")]
+    Version,
+    PackageVersion = Version, // Json name
+    [Description("License Information Origin")]
+    LicenseInformationOrigin,
+    [Description("License Expression")]
+    LicenseExpression,
+    [Description("License Url")]
+    LicenseUrl,
+    [Description("Copyright")]
+    Copyright,
+    [Description("Authors")]
+    Authors,
+    [Description("Package Project Url")]
+    PackageProjectUrl,
+    [Description("Error")]
+    Error,
+    ValidationErrors = Error, // Json name, group "Error" and "ErrorContext"
+    [Description("Error Context")]
+    ErrorContext,
+}
