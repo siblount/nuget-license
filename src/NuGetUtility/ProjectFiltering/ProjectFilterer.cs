8042490ffd683d570fdcc2654266842c40491dc5
Solomon Blount <74916907+siblount@users.noreply.github.com>
2025-03-07 17:01:09 -0500

    Add support for filtering shared VS projects

---
+++ src/NuGetUtility/ProjectFiltering/ProjectFilterer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetUtility.ProjectFiltering
+{
+    public class ProjectFilterer
+    {
+        /// <summary>
+        /// A singleton instance of this class.
+        /// </summary>
+        public static ProjectFilterer Instance { get; } = new ProjectFilterer();
+
+        /// <summary>
+        /// Filters a collection of project paths based on inclusion rules.
+        /// </summary>
+        /// <param name="projects">Collection of project paths to filter</param>
+        /// <param name="includeSharedProjects">Whether to include .shproj files</param>
+        /// <returns>Filtered collection of project paths</returns>
+        public IEnumerable<string> FilterProjects(IEnumerable<string> projects, bool includeSharedProjects)
+        {
+            return includeSharedProjects ? projects : projects.Where(p => !IsSharedProject(p));
+        }
+
+        /// <summary>
+        /// Determines if a project is a shared project based on file extension.
+        /// </summary>
+        /// <param name="projectPath">Path to the project file</param>
+        /// <returns>True if the project is a shared project, otherwise false</returns>
+        public bool IsSharedProject(string projectPath)
+        {
+            return projectPath.EndsWith(".shproj", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ tests/NuGetUtility.Test/ProjectFiltering/ProjectFiltererTest.cs
@@ -0,0 +1,60 @@
+﻿using NuGetUtility.ProjectFiltering;
+
+namespace NuGetUtility.Test.ProjectFiltering
+{
+    [TestFixture]
+    class ProjectFiltererTest
+    {
+        private ProjectFilterer _filterer = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _filterer = new ProjectFilterer();
+        }
+
+        [TestCase("test.shproj", true)]
+        [TestCase("test.SHPROJ", true)]
+        [TestCase("test.csproj", false)]
+        [TestCase("test.vbproj", false)]
+        public void IsSharedProject_DetectsProjectTypeCorrectly(string projectPath, bool expectedResult)
+        {
+            bool result = _filterer.IsSharedProject(projectPath);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void FilterProjects_ExcludesSharedProjects_WhenIncludeSharedProjectsIsFalse()
+        {
+            var projects = new[] { "one.csproj", "two.shproj", "three.csproj" };
+
+            var result = _filterer.FilterProjects(projects, false).ToArray();
+
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result, Does.Contain("one.csproj"));
+            Assert.That(result, Does.Contain("three.csproj"));
+            Assert.That(result, Does.Not.Contain("two.shproj"));
+        }
+
+        [Test]
+        public void FilterProjects_IncludesAllProjects_WhenIncludeSharedProjectsIsTrue()
+        {
+            var projects = new[] { "one.csproj", "two.shproj", "three.csproj" };
+
+            var result = _filterer.FilterProjects(projects, true).ToArray();
+
+            Assert.That(result.Count, Is.EqualTo(3));
+            Assert.That(result, Does.Contain("one.csproj"));
+            Assert.That(result, Does.Contain("two.shproj"));
+            Assert.That(result, Does.Contain("three.csproj"));
+        }
+
+        [Test]
+        public void Instance_Exists() {
+            var instance = ProjectFilterer.Instance;
+
+            Assert.That(instance, Is.Not.Null);
+        }
+    }
+}
